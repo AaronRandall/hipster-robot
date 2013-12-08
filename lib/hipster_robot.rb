caa88ed1e18c773588597b4ab9817d365e721f7f
Aaron Randall <aaronjrandall@gmail.com>
2013-12-08 13:22:57 +0000

    More tweaks to timings.

--- lib/hipster_robot.rb
+++ lib/hipster_robot.rb
@@ -47,7 +47,7 @@ class HipsterRobot
     @arm.perform_action(RoboticArm::ELBOW_UP, 2)
     @arm.perform_action(RoboticArm::BASE_RIGHT, 6.3)
     @arm.perform_action(RoboticArm::SHOULDER_DOWN, 1.4)
-    @arm.perform_action(RoboticArm::SHOULDER_UP, 1.6)
+    @arm.perform_action(RoboticArm::SHOULDER_UP, 1.63)
     @arm.perform_action(RoboticArm::BASE_LEFT, 6.2)
     @arm.perform_action(RoboticArm::ELBOW_DOWN, 1.7)
   end
