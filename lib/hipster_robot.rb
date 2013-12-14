21be712727e9ded7dfc66b892c3d4763602d0c9e
Aaron Randall <aaronjrandall@gmail.com>
2013-12-14 20:08:37 +0000

    Small refactor of hipster_robot code. Move robotic arm driver into lib directory.

--- lib/hipster_robot.rb
+++ lib/hipster_robot.rb
@@ -1,10 +1,10 @@
 class HipsterRobot
-  require_relative 'robotic_arm_driver/robotic_arm'
+  require_relative 'robotic_arm'
   require_relative 'artist_hipster_analyser'
   require 'net/http'
   require 'json'
 
-  DELAY_BETWEEN_REQUESTS_SECONDS = 3
+  DELAY_BETWEEN_REQUESTS_IN_SECONDS = 3
 
   def initialize
     @arm = RoboticArm.new
@@ -18,12 +18,10 @@ class HipsterRobot
 
       if response.body
         json_response = JSON.parse(response.body)
-        artist_name   = json_response['artistName']
-        track_name    = json_response['trackName']
+        @artist_name   = json_response['artistName']
+        @track_name    = json_response['trackName']
 
-        if ((artist_name != @last_artist_name) || (track_name != @last_track_name)) &&
-          (!artist_name.nil? && !track_name.nil?)
-
+        if artist_and_track_are_not_nil && artist_or_track_has_changed
           if @hipster_analyser.artist_is_not_hipster(artist_name)
             puts "Caught you listening to #{artist_name}, #{track_name}! Stop it!"
             stop_track
@@ -37,13 +35,17 @@ class HipsterRobot
         end
       end
 
-      sleep DELAY_BETWEEN_REQUESTS_SECONDS
+      sleep DELAY_BETWEEN_REQUESTS_IN_SECONDS
     end
   end
 
   private
 
   def stop_track
+    # The arm actions required to move the robot to press the spacebar.
+    # Note that the actions (e.g. ELBOW_UP) are not equal to the equivalent 
+    # reverse action (e.g. ELBOW_DOWN). This is because the arm's motors 
+    # have to work harder to perform against gravity
     @arm.perform_action(RoboticArm::ELBOW_UP, 2)
     @arm.perform_action(RoboticArm::BASE_RIGHT, 6.3)
     @arm.perform_action(RoboticArm::SHOULDER_DOWN, 1.4)
@@ -54,6 +56,14 @@ class HipsterRobot
 
   def close
     @arm.close
+  end
+
+  def artist_or_track_has_changed
+    return (@artist_name != @last_artist_name) || (@track_name != @last_track_name)
+  end
+
+  def artist_and_track_are_not_nil
+    return (!artist_name.nil? && !track_name.nil?)
   end
 
 end
